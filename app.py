f29f1bdc9d3671cdac0154f67e065b1b004709d0
zrihanroi <roi137cd@gmail.com>
2019-05-22 15:28:32 +0300

    done

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from endpoints.endpoints import endpoints
 app = Flask(__name__)
 app.register_blueprint(endpoints)
 
+@app.route('/')
+def landing():
+    return "<h1>Landing page</h1>"
+
 if __name__ == "__main__":
     print "Starting server"
     app.run(port = 4200)
